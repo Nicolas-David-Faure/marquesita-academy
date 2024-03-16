bc41baaa59b19c3192d833620c8dc1773711835d
Nicolas-David-Faure <faureee89@gmail.com>
2024-03-16 19:26:14 -0300

    se añade el banner en home con su logo

--- src/comp/Header.jsx
+++ src/comp/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "../styles/header.scss";
 import { NavMarquesita } from "./NavMarquesita";
-import logoMarquesita from "../assets/img/LogotipoACADEMY_Color_En_Negativo_JPG.jpg";
+import logoMarquesita from "../assets/img/marquesita_academy_logo.png";
 import logoInstagram from "../assets/img/instagramLogo.svg";
+
 export const Header = () => {
 
-  console.log(logoMarquesita, logoInstagram)
   return (
     <div className="header__container">
 
@@ -18,8 +18,8 @@ export const Header = () => {
       <NavMarquesita />
 
       <div className="header__instagram_container">
-        <strong>@marquesita.academy</strong>
-        <img src={logoInstagram} />
+        <a href="https://www.instagram.com/marquesita.20/?ref=playak.com&hl=pa" target="_blank">
+        <img src={logoInstagram} /></a>
       </div>
     </div>
   );
