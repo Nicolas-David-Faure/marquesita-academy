b1e1aae87f022857accab35296bb69b08c189233
Nicolas-David-Faure <faureee89@gmail.com>
2024-03-16 17:59:30 -0300

    add logos , banner, heade ,

---
+++ src/comp/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../styles/home.scss'
+export const Home = () => {
+  return (
+    <div className='home__container'>
+      
+      <div className='home__title'>
+
+        <h1>Marquesita</h1>
+
+        <h2>academy</h2>
+      </div>
+
+
+      <div className="home__promotion">
+          <span className="home__promotion__span">¡Inscribite ahora! </span>
+          <span className="home__promotion__span">y obtené un 10% de descuento</span>
+      </div>
+
+
+    </div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,15 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { App } from './App'
 
+import {
+  BrowserRouter
+} from "react-router-dom";
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App /> 
+    <BrowserRouter>
+      <App /> 
+    </BrowserRouter>
   </React.StrictMode>,
 )
